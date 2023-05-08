c3e0955b3fe8e03ca899983cb5196e687e06126a
Penguin.jpg <sky026912@gmail.com>
2023-05-08 18:37:37 +0800

    add todo-comments.nvim and fine-cmdline.nvim plugins

--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,19 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+    event = "User AstroFile",
+    cmd = { "TodoQuickFix" },
+  },
+  {
+    "VonHeikemen/fine-cmdline.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    opts = {},
+    event = "VeryLazy",
+    keys = { "<C-S-t>", "<cmd>FineCmdline<cr>", desc = "Open fine-cmdline" },
+  }
 }
