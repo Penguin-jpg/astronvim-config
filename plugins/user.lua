ee689f9579506906396e050d5c2884160cd8ea77
Penguin.jpg <sky026912@gmail.com>
2023-05-08 19:43:57 +0800

    remove fine-cmdline, change to wilder

--- plugins/user.lua
+++ plugins/user.lua
@@ -18,10 +18,31 @@ return {
     cmd = { "TodoQuickFix" },
   },
   {
-    "VonHeikemen/fine-cmdline.nvim",
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {},
+    "gelguy/wilder.nvim",
     event = "VeryLazy",
-    keys = { "<C-S-t>", "<cmd>FineCmdline<cr>", desc = "Open fine-cmdline" },
-  }
+    config = function()
+      local wilder = require "wilder"
+      -- basic setup
+      wilder.setup { modes = { ":", "/", "?" } }
+
+      -- popup commandline
+      wilder.set_option(
+        "renderer",
+        wilder.popupmenu_renderer(wilder.popupmenu_palette_theme {
+          -- 'single', 'double', 'rounded' or 'solid'
+          -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+          border = "rounded",
+          max_height = "50%",      -- max height of the palette
+          min_height = 0,
+          prompt_position = "top", -- 'top' or 'bottom' to set the location of the prompt
+          reverse = 0,             -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+          highlighter = wilder.basic_highlighter(),
+          left = { " ", wilder.popupmenu_devicons() },
+          right = { " ", wilder.popupmenu_scrollbar() },
+          pumblend = 20,
+        })
+      )
+    end,
+  },
+
 }
