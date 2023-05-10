73a49daaeaf2f0eb845a9ea2323b136656372e8f
Penguin.jpg <sky026912@gmail.com>
2023-05-10 21:28:19 +0800

    update statusline config

--- init.lua
+++ init.lua
@@ -97,12 +97,8 @@ return {
 
   -- add new user interface icon
   icons = {
-    VimIcon = "",
     ScrollText = "",
     GitBranch = "",
-    GitAdd = "",
-    GitChange = "",
-    GitDelete = "",
   },
   -- modify variables used by heirline but not defined in the setup call directly
   heirline = {
@@ -110,17 +106,11 @@ return {
     separators = {
       left = { "", " " }, -- separator for the left side of the statusline
       right = { " ", "" }, -- separator for the right side of the statusline
-      tab = { "", "" },
     },
     -- add new colors that can be used by heirline
     colors = function(hl)
       local get_hlgroup = require("astronvim.utils").get_hlgroup
       -- use helper function to get highlight group properties
-      local comment_fg = get_hlgroup("Comment").fg
-      hl.git_branch_fg = comment_fg
-      hl.git_added = comment_fg
-      hl.git_changed = comment_fg
-      hl.git_removed = comment_fg
       hl.blank_bg = get_hlgroup("Folded").fg
       hl.file_info_bg = get_hlgroup("Visual").bg
       hl.nav_icon_bg = get_hlgroup("String").fg
