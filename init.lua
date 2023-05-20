1adcd0499d14b0662f95144717e4df948fa37064
Penguin.jpg <sky026912@gmail.com>
2023-05-20 17:52:01 +0800

    add a small space to heirline left separator

--- init.lua
+++ init.lua
@@ -109,7 +109,7 @@ return {
   heirline = {
     -- define the separators between each section
     separators = {
-      left = { "", "" }, -- separator for the left side of the statusline
+      left = { "", " " }, -- separator for the left side of the statusline
       right = { " ", "" }, -- separator for the right side of the statusline
     },
     -- add new colors that can be used by heirline
