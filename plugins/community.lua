fd051130f00ec8b2cfee6371641cead5e9da4ab2
Penguin.jpg <sky026912@gmail.com>
2023-09-24 15:03:55 +0800

    back to clangd_extensions only

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,5 +5,5 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   -- Language pack
-  { import = "astrocommunity.pack.cpp" },
+  -- { import = "astrocommunity.pack.cpp" },
 }
