af680c983acf5b24d033a0ca3b8851316083f282
Penguin.jpg <sky026912@gmail.com>
2023-09-24 14:12:22 +0800

    remove force zig compiler

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -2,12 +2,6 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
-
-    -- use zig as compiler for windows to work :P
-    if vim.fn.has "win32" then
-      require("nvim-treesitter.install").compilers = { "zig" }
-    end
-
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "lua",
       "c",
