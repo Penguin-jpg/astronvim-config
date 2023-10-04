ede6f6f31aa62acbfb5ebe36201d7babdf891a5c
Penguin.jpg <sky026912@gmail.com>
2023-10-05 00:48:58 +0800

    update clangd options

--- init.lua
+++ init.lua
@@ -57,6 +57,21 @@ return {
         capabilities = {
           offsetEncoding = "utf-8",
         },
+        cmd = {
+          "clangd",
+          "--background-index"
+          "--suggest-missing-includes",
+          "--clang-tidy",
+          "--all-scopes-completion",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--fallback-style=Microsoft",
+        },
+        init_options = {
+          clangdFileStatus = true,
+          usePlaceholders = false,
+          completeUnimported = true,
+        },
       },
     },
   },
