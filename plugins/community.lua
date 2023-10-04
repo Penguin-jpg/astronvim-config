eaf43fc28ba8d254304cbcf2d5f3aa6703a2ffa5
Penguin.jpg <sky026912@gmail.com>
2023-10-04 15:47:43 +0800

    reset flash.nvim to default keymapping or it just doesn't work

--- plugins/community.lua
+++ plugins/community.lua
@@ -15,36 +15,4 @@ return {
   { import = "astrocommunity.diagnostics.trouble-nvim"},
   -- Faster navigation, character motions
   { import = "astrocommunity.motion.flash-nvim" },
-  {
-    "folke/flash.nvim",
-    event = "verylazy",
-    vscode = true,
-    opts = {},
-    keys = {
-      {
-        "<C-f>",
-        mode = { "n", "x", "o" },
-        function() require("flash").jump() end,
-        desc = "flash",
-      },
-      {
-        "S",
-        mode = { "n", "o", "x" },
-        function() require("flash").treesitter() end,
-        desc = "flash treesitter",
-      },
-      {
-        "r",
-        mode = "o",
-        function() require("flash").remote() end,
-        desc = "remote flash",
-      },
-      {
-        "R",
-        mode = { "o", "x" },
-        function() require("flash").treesitter_search() end,
-        desc = "treesitter search",
-      },
-    },
-  },
 }
