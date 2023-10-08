98535927020c7bb8bac42e7b9d42348ddf6374a5
Penguin.jpg <sky026912@gmail.com>
2023-10-08 23:51:00 +0800

    add codeium.vim to assist coding

--- plugins/user.lua
+++ plugins/user.lua
@@ -95,4 +95,22 @@ return {
       return vim.list_extend(mappings, keys)
     end,
   },
+  -- AI copilot
+  {
+    "Exafunction/codeium.vim",
+    event = "User AstroFile",
+    config = function()
+      vim.keymap.set("i", "<Tab>", function() return vim.fn["codeium#Accept"]() end, { expr = true })
+      vim.keymap.set("i", "<A-]>", function() return vim.fn["codeium#CycleCompletions"](1) end, { expr = true })
+      vim.keymap.set("i", "<A-[>", function() return vim.fn["codeium#CycleCompletions"](-1) end, { expr = true })
+      vim.keymap.set("i", "<C-x>", function() return vim.fn["codeium#Clear"]() end, { expr = true })
+      vim.keymap.set("n", "<leader>;", function()
+        if vim.g.codeium_enabled == true then
+          vim.cmd "CodeiumDisable"
+        else
+          vim.cmd "CodeiumEnable"
+        end
+      end, { noremap = true, desc = "Toggle Codeium active" })
+    end,
+  },
 }
