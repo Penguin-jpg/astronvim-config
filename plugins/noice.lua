c1d035f69587c93adaa27b7ef51bbf9bdd23bac4
Penguin.jpg <sky026912@gmail.com>
2023-10-12 00:59:55 +0800

    disable more notifications from noice.nvim

--- plugins/noice.lua
+++ plugins/noice.lua
@@ -1,18 +1,35 @@
 return {
   "folke/noice.nvim",
-  config = function()
-    require("noice").setup {
-      -- Disable "written" message
+  opts = function(_, opts)
+    return require("astronvim.utils").extend_tbl(opts, {
+      -- Disable lsp related notificaitons
+      lsp = {
+        progress = { enabled = false },
+        hover = { enabled = false },
+        signature = { enabled = false },
+      },
       routes = {
+        -- Disable "written" notification
         {
-          filter = {
-            event = "msg_show",
-            kind = "",
-            find = "written",
-          },
+          filter = { event = "msg_show", kind = "", find = "written" },
+          opts = { skip = true },
+        },
+        -- Disable paste notification
+        {
+          filter = { event = "msg_show", find = "^%d+ more lines$" },
+          opts = { skip = true },
+        },
+        -- Disable delete notification
+        {
+          filter = { event = "msg_show", find = "^%d+ fewer lines$" },
+          opts = { skip = true },
+        },
+        -- Disable yank notification
+        {
+          filter = { event = "msg_show", find = "^%d+ lines yanked$" },
           opts = { skip = true },
         },
       },
-    }
+    })
   end,
 }
