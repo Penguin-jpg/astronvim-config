d27142801617a226b304d83702f4796a3a4aebbd
Penguin.jpg <sky026912@gmail.com>
2023-10-13 23:34:28 +0800

    update mappings

--- mappings.lua
+++ mappings.lua
@@ -25,13 +25,7 @@ maps.n["<leader>bD"] = {
 -- this is useful for naming menus
 maps.n["<leader>b"] = { name = "Buffers" }
 
--- Resize window
-maps.n["<C-A-Up>"] = { function() require("smart-splits").resize_up() end, desc = "Resize split up" }
-maps.n["<C-A-Down>"] = { function() require("smart-splits").resize_down() end, desc = "Resize split down" }
-maps.n["<C-A-Left>"] = { function() require("smart-splits").resize_left() end, desc = "Resize split left" }
-maps.n["<C-A-Right>"] = { function() require("smart-splits").resize_right() end, desc = "Resize split right" }
-
--- Utility functions
+------ Utility functions ------
 maps.n["<C-a>"] = { "ggVG", desc = "Select all lines" }
 if is_available "Comment.nvim" then
   maps.n["<C-_>"] = {
@@ -59,6 +53,11 @@ maps.i["<C-r>"] = { "<C-o><C-r>", desc = "Redo" }
 maps.i["<C-s>"] = { "<cmd>w!<cr>", desc = "Save file" } 
 maps.v["<C-c>"] = { "y", desc = "Copy selected block" }
 maps.i["<S-Tab>"] = { "<C-d>", desc = "Unindent line" }
+-- Delete without yanking
+maps.n["D"] = { '"_dd', desc = "Delete without yanking" }
+maps.v["D"] = { '"_d', desc = "Delete without yanking" }
+
+-- Custom mappings for nvim-notify
 if is_available "nvim-notify" then
   maps.n["<leader>un"] = {
     function() require("notify").dismiss { silent = true, pending = true } end,
@@ -66,7 +65,7 @@ if is_available "nvim-notify" then
   }
 end
 
--- Custom mappings for nvim-gomove
+------ Custom mappings for nvim-gomove ------
 if is_available "nvim-gomove" then
   maps.n["<A-Up>"] = { "<Plug>GoNSMUp", desc = "Move line up" }
   maps.n["<A-Down>"] = { "<Plug>GoNSMDown", desc = "Move line down" }
@@ -82,7 +81,7 @@ if is_available "nvim-gomove" then
   maps.v["<S-Down>"] = { "<Plug>GoVSDDown", desc = "Duplicate block and paste it below" }
 end
 
--- Custom mappings for word/line substitution
+------ Custom mappings for substitution.nvim ------
 if is_available "substitute.nvim" then
   maps.n["<leader>r"] = sections.r
   maps.n["<leader>rr"] = {
