ad281f4914376de87f72ee9b58dc9e35884073bb
Penguin.jpg <sky026912@gmail.com>
2023-10-13 23:17:50 +0800

    move additional setup/config to core.lua

--- plugins/toggleterm.lua
+++
@@ -1,9 +0,0 @@
--- Use powershell for toggleterm
-return {
-  "akinsho/toggleterm.nvim",
-  opts = function(_, opts)
-    if vim.fn.has "win32" then
-      opts.shell = "pwsh.exe"
-    end
-  end
-}
