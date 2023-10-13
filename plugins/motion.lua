b82c8fdc682975e094c8b23e61a5f44e6f119db4
Penguin.jpg <sky026912@gmail.com>
2023-10-13 23:00:34 +0800

    disable winbar, move everything in movement.lua to motion.lua

--- plugins/motion.lua
+++ plugins/motion.lua
@@ -1,5 +1,22 @@
 -- Plugins related to motion
 return {
+  -- Move/duplicate lines up/down/left/right
+  {
+    "booperlv/nvim-gomove",
+    event = "User AstroFile",
+    config = function()
+      require("gomove").setup {
+        -- whether or not to map default key bindings, (true/false)
+        map_defaults = false,
+        -- whether or not to reindent lines moved vertically (true/false)
+        reindent = true,
+        -- whether or not to undojoin same direction moves (true/false)
+        undojoin = true,
+        -- whether to not to move past end column when moving blocks horizontally, (true/false)
+        move_past_end_col = false,
+      }
+    end,
+  },
   -- Better move by word support
   {
     "chaoren/vim-wordmotion",
